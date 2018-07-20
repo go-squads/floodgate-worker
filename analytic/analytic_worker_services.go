1b03a96f22a2ebb28e42f3a3a4f18c89f52f3088
Vincent Surya Bensawan <bensawan0298@gmail.com>
2018-07-20 14:03:52 +0700

    Deleted the previous interface since it's not neccessary

--- analytic/analytic_worker_services.go
+++ analytic/analytic_worker_services.go
@@ -7,11 +7,13 @@ import (
 	cluster "github.com/bsm/sarama-cluster"
 )
 
+const (
+	GroupID = "StreamAnalyser"
+)
+
 type analyticServices struct {
-	Client         *sarama.Client
-	Config         *cluster.Config
-	AnalyticConfig *AnalyticConfig
-	GroupID        string
+	clusterConfig cluster.Config
+	Client        sarama.Client
 }
 
 func (a *analyticServices) SetUpConfig() *cluster.Config {
@@ -23,10 +25,25 @@ func (a *analyticServices) SetUpConfig() *cluster.Config {
 	return clusterConfig
 }
 
-func (a *analyticServices) SetUpClient(brokers []string, config *sarama.Config) (*sarama.Client, error) {
-	client, err := *sarama.NewClient(brokers, config)
-	if err != nil {
-		log.Printf("Failed to connect to broker")
-	}
+func (a *analyticServices) SetUpClient(brokers []string, config *sarama.Config) (sarama.Client, error) {
+	client, err := sarama.NewClient(brokers, config)
 	return client, err
+}
+
+// Call NewConsumer
+func (a *analyticServices) SpawnAnalyticWorker(brokers []string) {
+	brokerConfig := sarama.NewConfig()
+	analyserClusterConfig := a.SetUpConfig()
+	brokerClient, err := a.SetUpClient(brokers, brokerConfig)
+	if err != nil {
+		log.Printf("Failed to connect to broker...")
+	}
+	topicList, err := brokerClient.Topics()
+	workerCluster, err := cluster.NewConsumer(brokers, GroupID, topicList, analyserClusterConfig)
+	if err != nil {
+		log.Printf("Failed to init consumer")
+	}
+	// use worker made to call NewAnalyticWorker
+	worker := NewAnalyticWorker(workerCluster)
+	worker.Start()
 }
